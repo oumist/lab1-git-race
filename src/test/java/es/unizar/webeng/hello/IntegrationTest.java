47f7fde78a9859a2c5ef892edd7a178c90c3ae73
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2020-09-15 14:23:17 +0000

    Fix bad import

--- src/test/java/es/unizar/webeng/hello/IntegrationTest.java
+++ src/test/java/es/unizar/webeng/hello/IntegrationTest.java
@@ -2,7 +2,7 @@ package es.unizar.webeng.hello;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
