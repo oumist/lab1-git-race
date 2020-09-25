6bbdc9731071c08d96ae8565869af9f4333baa33
jgarciapueyo <jgarciapueyo@gmail.com>
2020-09-25 09:16:07 +0200

    Uses WebClient for async request. Cleanup of Application.java and dependencies in build.gradle

--- src/main/java/es/unizar/webeng/hello/TweetController.java
+++ src/main/java/es/unizar/webeng/hello/TweetController.java
@@ -1,6 +1,6 @@
 package es.unizar.webeng.hello;
 
-import com.google.gson.*;
+import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+import reactor.core.publisher.Flux;
 
 /**
  * Controller for Twitter Timeline.
@@ -56,16 +55,14 @@ public class TweetController {
                 .build();
 
         try {
-            String res = client.get()
-                    //.uri("/users/by?usernames="+user)
+            Flux<Tweet> res = client.get()
                     .uri("?screen_name=" + user + "&count=5")
                     .header("authorization", "Bearer " + twitter_bearer)
                     .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
+                    .bodyToFlux(Tweet.class);
 
             model.addAttribute("user", user);
-            model.addAttribute("tweets", transformResponse(res, "text"));
+            model.addAttribute("tweets", new ReactiveDataDriverContextVariable(res, 5));
 
         } catch (WebClientResponseException wcre) {
             if (wcre.getRawStatusCode() == 404) {
@@ -83,34 +80,79 @@ public class TweetController {
     }
 
     /**
-     * Transforms the response of Twitter API into a list of tweets.
-     * A tweet is composed of the text of the tweet.
-     *
-     * @param res   Twitter API response in JSON format
-     * @param field the json field to retrieve from the response
-     * @return list of the tweets
-     */
-    private List<Tweet> transformResponse(String res, String field) {
-        Gson gson = new Gson();
-        JsonArray jsonArray = gson.fromJson(res, JsonArray.class);
-        List<Tweet> tweetList = new ArrayList<Tweet>();
-
-        for (JsonElement jsonElement : jsonArray) {
-            tweetList.add(new Tweet(jsonElement.getAsJsonObject().get(field).getAsString()));
-        }
-
-        return tweetList;
-    }
-
-    /**
      * Representation of a tweet.
      * It contains only the text of a tweet.
+     * <p>
+     * It could be improved with annotations from <a href="https://projectlombok.org/">Project Lombok</a> not to have to
+     * write constructors, getters and setters.
      */
-    class Tweet {
-        private String text;
+    public static class Tweet {
 
-        public Tweet(String text) {
+        private String created_at;
+        private String id;
+        private String id_str;
+        public String text;
+        private Boolean truncated;
+        private JsonNode entities;
+        private String source;
+        private String in_reply_to_status_id;
+        private String in_reply_to_status_id_str;
+        private String in_reply_to_user_id;
+        private String in_reply_to_user_id_str;
+        private String in_reply_to_screen_name;
+        private JsonNode user;
+        private String geo;
+        private String coordinates;
+        private String place;
+        private String contributors;
+        private Boolean is_quote_status;
+        private String quoted_status_id;
+        private String quoted_status_id_str;
+        private JsonNode quoted_status;
+        private String retweet_count;
+        private String favorite_count;
+        private String favorited;
+        private String retweeted;
+        private String possibly_sensitive;
+        private String lang;
+
+        public Tweet() {
+        }
+
+        public Tweet(String created_at, String id, String id_str, String text, Boolean truncated, JsonNode entities,
+                     String source, String in_reply_to_status_id, String in_reply_to_status_id_str,
+                     String in_reply_to_user_id, String in_reply_to_user_id_str, String in_reply_to_screen_name,
+                     JsonNode user, String geo, String coordinates, String place, String contributors,
+                     Boolean is_quote_status, String quoted_status_id, String quoted_status_id_str,
+                     JsonNode quoted_status, String retweet_count, String favorite_count, String favorited,
+                     String retweeted, String possibly_sensitive, String lang) {
+            this.created_at = created_at;
+            this.id = id;
+            this.id_str = id_str;
             this.text = text;
+            this.truncated = truncated;
+            this.entities = entities;
+            this.source = source;
+            this.in_reply_to_status_id = in_reply_to_status_id;
+            this.in_reply_to_status_id_str = in_reply_to_status_id_str;
+            this.in_reply_to_user_id = in_reply_to_user_id;
+            this.in_reply_to_user_id_str = in_reply_to_user_id_str;
+            this.in_reply_to_screen_name = in_reply_to_screen_name;
+            this.user = user;
+            this.geo = geo;
+            this.coordinates = coordinates;
+            this.place = place;
+            this.contributors = contributors;
+            this.is_quote_status = is_quote_status;
+            this.quoted_status_id = quoted_status_id;
+            this.quoted_status_id_str = quoted_status_id_str;
+            this.quoted_status = quoted_status;
+            this.retweet_count = retweet_count;
+            this.favorite_count = favorite_count;
+            this.favorited = favorited;
+            this.retweeted = retweeted;
+            this.possibly_sensitive = possibly_sensitive;
+            this.lang = lang;
         }
 
         public String getText() {
@@ -119,6 +161,214 @@ public class TweetController {
 
         public void setText(String text) {
             this.text = text;
+        }
+
+        public String getCreated_at() {
+            return created_at;
+        }
+
+        public void setCreated_at(String created_at) {
+            this.created_at = created_at;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getId_str() {
+            return id_str;
+        }
+
+        public void setId_str(String id_str) {
+            this.id_str = id_str;
+        }
+
+        public Boolean getTruncated() {
+            return truncated;
+        }
+
+        public void setTruncated(Boolean truncated) {
+            this.truncated = truncated;
+        }
+
+        public JsonNode getEntities() {
+            return entities;
+        }
+
+        public void setEntities(JsonNode entities) {
+            this.entities = entities;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public void setSource(String source) {
+            this.source = source;
+        }
+
+        public String getIn_reply_to_status_id() {
+            return in_reply_to_status_id;
+        }
+
+        public void setIn_reply_to_status_id(String in_reply_to_status_id) {
+            this.in_reply_to_status_id = in_reply_to_status_id;
+        }
+
+        public String getIn_reply_to_status_id_str() {
+            return in_reply_to_status_id_str;
+        }
+
+        public void setIn_reply_to_status_id_str(String in_reply_to_status_id_str) {
+            this.in_reply_to_status_id_str = in_reply_to_status_id_str;
+        }
+
+        public String getIn_reply_to_user_id() {
+            return in_reply_to_user_id;
+        }
+
+        public void setIn_reply_to_user_id(String in_reply_to_user_id) {
+            this.in_reply_to_user_id = in_reply_to_user_id;
+        }
+
+        public String getIn_reply_to_user_id_str() {
+            return in_reply_to_user_id_str;
+        }
+
+        public void setIn_reply_to_user_id_str(String in_reply_to_user_id_str) {
+            this.in_reply_to_user_id_str = in_reply_to_user_id_str;
+        }
+
+        public String getIn_reply_to_screen_name() {
+            return in_reply_to_screen_name;
+        }
+
+        public void setIn_reply_to_screen_name(String in_reply_to_screen_name) {
+            this.in_reply_to_screen_name = in_reply_to_screen_name;
+        }
+
+        public JsonNode getUser() {
+            return user;
+        }
+
+        public void setUser(JsonNode user) {
+            this.user = user;
+        }
+
+        public String getGeo() {
+            return geo;
+        }
+
+        public void setGeo(String geo) {
+            this.geo = geo;
+        }
+
+        public String getCoordinates() {
+            return coordinates;
+        }
+
+        public void setCoordinates(String coordinates) {
+            this.coordinates = coordinates;
+        }
+
+        public String getPlace() {
+            return place;
+        }
+
+        public void setPlace(String place) {
+            this.place = place;
+        }
+
+        public String getContributors() {
+            return contributors;
+        }
+
+        public void setContributors(String contributors) {
+            this.contributors = contributors;
+        }
+
+        public Boolean getIs_quote_status() {
+            return is_quote_status;
+        }
+
+        public void setIs_quote_status(Boolean is_quote_status) {
+            this.is_quote_status = is_quote_status;
+        }
+
+        public String getQuoted_status_id() {
+            return quoted_status_id;
+        }
+
+        public void setQuoted_status_id(String quoted_status_id) {
+            this.quoted_status_id = quoted_status_id;
+        }
+
+        public String getQuoted_status_id_str() {
+            return quoted_status_id_str;
+        }
+
+        public void setQuoted_status_id_str(String quoted_status_id_str) {
+            this.quoted_status_id_str = quoted_status_id_str;
+        }
+
+        public JsonNode getQuoted_status() {
+            return quoted_status;
+        }
+
+        public void setQuoted_status(JsonNode quoted_status) {
+            this.quoted_status = quoted_status;
+        }
+
+        public String getRetweet_count() {
+            return retweet_count;
+        }
+
+        public void setRetweet_count(String retweet_count) {
+            this.retweet_count = retweet_count;
+        }
+
+        public String getFavorite_count() {
+            return favorite_count;
+        }
+
+        public void setFavorite_count(String favorite_count) {
+            this.favorite_count = favorite_count;
+        }
+
+        public String getFavorited() {
+            return favorited;
+        }
+
+        public void setFavorited(String favorited) {
+            this.favorited = favorited;
+        }
+
+        public String getRetweeted() {
+            return retweeted;
+        }
+
+        public void setRetweeted(String retweeted) {
+            this.retweeted = retweeted;
+        }
+
+        public String getPossibly_sensitive() {
+            return possibly_sensitive;
+        }
+
+        public void setPossibly_sensitive(String possibly_sensitive) {
+            this.possibly_sensitive = possibly_sensitive;
+        }
+
+        public String getLang() {
+            return lang;
+        }
+
+        public void setLang(String lang) {
+            this.lang = lang;
         }
     }
 }
