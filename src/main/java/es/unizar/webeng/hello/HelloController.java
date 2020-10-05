379f9c92969aa942767f72266fdf042500b2b2f8
sergiomartinez_1997@hotmail.com <sergiomartinez_1997@hotmail.com>
2020-10-05 19:52:01 +0200

    Added temperature information at Zaragoza

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 import java.util.Date;
 import java.util.Map;
 import java.util.Random;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 
 import io.swagger.annotations.Api;
@@ -33,6 +35,7 @@ public class HelloController {
     @Value("${app.joke_const:Hello World}")
     private String joke_const;
     private String link = "Abre este enlace";
+    private String weather_API = "http://api.openweathermap.org/data/2.5/forecast?id=3104324&appid=3974461a1f67f51495d6c273db02ccaf&lang=es";
 
     private String luckyColor(){
         
@@ -115,7 +118,33 @@ public class HelloController {
             model.put("host", "Cannot get the host name");
             model.put("ip", "Cannot get the host address or IP");
         }
-        
+
+
+        //OpenWeather API
+        try{
+
+            StringBuilder result = new StringBuilder();
+            URL url = new URL(weather_API);
+            URLConnection conn = url.openConnection();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null){
+                result.append(line);
+            }
+            rd.close();
+
+            String dataResult = new String(result);
+            String temperature = dataResult.substring(dataResult.indexOf("temp") + 6,
+                    dataResult.indexOf("feels_like") - 2);
+            System.out.println(temperature);
+            //Temperature is received in Kelvin, we use it in Celsius
+            int intTemperature = Integer.parseInt(temperature) - 273;
+            model.put("temperature", intTemperature);
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
         return "wellcome";
     }
 
